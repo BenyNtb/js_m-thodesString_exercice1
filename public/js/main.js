d9b7971e6c90559e426d6186e9eba3deec1c9ed3
Beny <b.ntambwe97@gmail.com>
2020-12-29 12:04:21 +0100

    fin exo

---
+++ public/js/main.js
@@ -0,0 +1,11 @@
+console.log(phrase.slice(8, 45));
+console.log(phrase.slice(0, 45));
+console.log(phrase.slice(0, 7));
+
+let phrase2 = "Ceci est une variable de type number !"
+const regex = /number/gi;
+console.log(phrase2.replace(regex, 'string'));
+
+let phrase3 = "Ceci est une variable de type NuMbEr ! Les numbers c'est trop bien Number numBer numbeR"
+
+console.log(phrase3.toLowerCase());
